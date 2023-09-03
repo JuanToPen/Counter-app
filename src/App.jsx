3efc30f6c5ef506345d530b08a8777ee45d90807
JuanToPen <juantolgap@gmail.com>
2023-09-03 17:35:16 +0200

    yurr

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,68 @@
 import { useState } from "react";
+import minus from "./imgs/minus.png";
+import plus from "./imgs/plus.png";
+import reload from "./imgs/reload.png";
+import settings from "./imgs/settings.png";
 
 function App() {
+  let [count, setCount] = useState(0);
+  let [isVisible, setIsVisible] = useState(false);
+
+  function increment() {
+    setCount((count += 1));
+  }
+
+  function decrement() {
+    setCount((count -= 1));
+  }
+
   return (
     <div id="body">
       <div id="options">
-        <p id="refresh">↻</p>
-        <p id="settings">⚙</p>
+        <img
+          id="refresh"
+          src={reload}
+          onClick={() => {
+            setCount(0);
+          }}
+        />
+        <img
+          id="settings"
+          src={settings}
+          onClick={() => {
+            setIsVisible(true);
+          }}
+        />
       </div>
       <div id="content">
-        <p id="decrement">-</p>
-        <div id="display">0</div>
-        <p id="increment">+</p>
+        <img id="decrement" src={minus} onClick={decrement} />
+        <p id="display">{count}</p>
+        <img id="increment" src={plus} onClick={increment} />
       </div>
+      {isVisible && (
+        <div id="settingsModal">
+          <div id="settingsContent">
+            <button
+              onClick={() => {
+                setIsVisible(false);
+              }}
+            >
+              ✕
+            </button>
+            <p>
+              SET COUNT: <input type="number" id="setCount" />
+            </p>
+            <div id="colors">
+              <div id="red" className="color"></div>
+              <div id="blue" className="color"></div>
+              <div id="green" className="color"></div>
+              <div id="yellow" className="color"></div>
+              <div id="orange" className="color"></div>
+              <div id="purple" className="color"></div>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
